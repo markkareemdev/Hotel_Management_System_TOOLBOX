08691a620132f57c1dcc3940ca9068c85cc8dee6
olawale-kareem <olawale.kareem@decagon.dev>
2021-08-25 05:13:37 +0100

    The base templates implementation

--- hotel/urls.py
+++ hotel/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [  
-    path('', views.hotel, name = 'hotel')
+    path('home/',views.home, name = 'home'),
+    path('home/about/',views.about, name = 'about'),
+    path('home/accomodation/',views.accomodation, name = 'accomodation'),
+    path('home/contact/',views.contact, name = 'contact'),
+    path('home/login/',views.login, name = 'login'),
+    path('home/register/',views.register, name = 'register')
 ]
+
+
